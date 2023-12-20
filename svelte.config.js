65ceeb0ee4d0706d826f6eba9541828b744dbbde
Hugo Castro <39707889+HugoCL@users.noreply.github.com>
2023-12-20 20:06:54 +0000

    added cloudflare deps and settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
 		}),
 		paths: {
 			relative: false
